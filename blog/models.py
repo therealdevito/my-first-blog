572e3b70d8998c5709218bef92ab2660af44840f
therealdevito <rucsandoiu.andrei@gmail.com>
2020-08-31 12:42:55 +0300

    comments count bug fixed

--- blog/models.py
+++ blog/models.py
@@ -16,6 +16,9 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
+    def approved_comments(self):
+        return self.comments.filter(approved_comment=True)
+
 class Comment(models.Model):
     post = models.ForeignKey('blog.Post', on_delete = models.CASCADE, related_name = 'comments')
     author = models.CharField(max_length = 200)
@@ -30,5 +33,4 @@ class Comment(models.Model):
     def __str__(self):
         return self.text
 
-    def approved_comments(self):
-        return self.comments.filter(approved_comment=True)
+   
